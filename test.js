25a2016b9e7d1906e9c4b4986181a4833733facd
Fr. John Lamansky <lamansky@users.noreply.github.com>
2019-11-15 10:51:17 +0100

    Support setting object descriptors

--- test.js
+++ test.js
@@ -25,6 +25,10 @@ describe('newObject()', function () {
     assert.strictEqual(Object.keys(obj).length, 0)
   })
 
+  it('should throw a TypeError if a non-object is given instead of entries', function () {
+    assert.throws(() => { newObject(123) }, TypeError)
+  })
+
   it('should throw a TypeError if an entry is not an object', function () {
     assert.throws(() => { newObject(['a', 'b']) }, TypeError)
   })
@@ -33,5 +37,42 @@ describe('newObject()', function () {
     newObject([[1, 1], [1, 1]])
     assert.throws(() => { newObject([[1, 1], [1, 1]], {throwIfEquivKeys: true}) }, TypeError)
     assert.throws(() => { newObject([[1, 1], [1, 1]], {throwIfEquivKeys: new RangeError()}) }, RangeError)
+  })
+
+  it('should accept descriptors instead of values if `descs` is `true`', function () {
+    const obj = newObject([['a', {value: 1}], ['b', {value: 2}]], {descs: true})
+    assert.strictEqual(Object.keys(obj).length, 0) // because they are non-enumerable
+    assert.strictEqual(obj.a, 1)
+    assert.strictEqual(obj.b, 2)
+  })
+
+  it('should accept an object of descriptors', function () {
+    const obj = newObject({a: {value: 1}, b: {value: 2}})
+    assert.strictEqual(Object.keys(obj).length, 0) // because they are non-enumerable
+    assert.strictEqual(obj.a, 1)
+    assert.strictEqual(obj.b, 2)
+  })
+
+  it('should support specifying default descriptor settings with `descDefaults`', function () {
+    const obj = newObject({a: {value: 1}, b: {value: 2}}, {descDefaults: {enumerable: true}})
+    assert.strictEqual(Object.keys(obj).length, 2)
+    assert.strictEqual(obj.a, 1)
+    assert.strictEqual(obj.b, 2)
+  })
+
+  it('should skip merging `writable` into a descriptor with `get` or `set`', function () {
+    const obj = newObject(
+      {a: {get: () => 1}, b: {value: 2}, c: {set: x => {}}},
+      {descDefaults: {enumerable: true, writable: true}}
+    )
+    assert.strictEqual(Object.keys(obj).length, 3)
+    assert.strictEqual(obj.a, 1)
+    assert.strictEqual(obj.b, 2)
+    assert.throws(() => {
+      obj.a = 123 // This should throw, but just in case:
+      if (obj.a === 123) throw new Error()
+    })
+    obj.b = 123
+    assert.strictEqual(obj.b, 123)
   })
 })
